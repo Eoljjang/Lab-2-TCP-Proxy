555bfa90022b54ed17c99cb5ad16162a1ba8773d
Nathan Wong <99845938+Eoljjang@users.noreply.github.com>
2023-09-21 17:01:43 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,19 +1,26 @@
 import socket
 
-BYTES_TO_READ = 4096
+BYTES_TO_READ = 4096 # How much data you're willing to accept.
 
 def get(host, port):
   request = b"GET/ HTTP/1.1\nHost: " + host.encode('utf-8') + b"\n\n"
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((host, port)) # Connect to server
-  s.send(request) # Send request
-  s.shutdown(socket.SHUT_WR) # "I'm done sending requests"
-  result = s.recv(BYTES_TO_READ) # Keep reading incoming data (from server)
+  # 1) Intialize socket
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+  # 2) Connect to server & make request
+  s.connect((host, port)) 
+  s.send(request)
+
+  # 3) State that "you're done sending requests"
+  s.shutdown(socket.SHUT_WR) 
+
+  # 4) Continuously receiving the response from server.# Keep reading incoming data (from server)
   while(len(result) > 0): # Keep reading the response until the sent response is empty.
     print(result)
     result = s.recv(BYTES_TO_READ)
 
+  # 5) Close the socket.
   s.close()
 
-#get("www.google.com", 80)
+#get("www.google.com", 80) # GET function that makes a request to google.com with code 80.
 get("localhost", 8080)
